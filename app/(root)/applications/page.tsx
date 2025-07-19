78440e3149820e309cf6a4c018c33c3eb6d5640f
g-m007 <gauthamhari0@gmail.com>
2025-07-19 14:04:02 +0530

    Fixed Applications page

--- app/(root)/applications/page.tsx
+++ app/(root)/applications/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = "force-dynamic";
 import { useEffect, useState } from 'react';
 import { getJobApplicationsByJobId, getRecruiterDetails, getJobDetailsById } from '@/server';
 import { useRouter, useSearchParams } from 'next/navigation';
